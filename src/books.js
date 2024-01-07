c3fc69f787b5cbdcb297bd97841f216c0d89cf50
jayrboy <j.jayox_@hotmail.com>
2024-01-08 00:28:29 +0700

    Local Images

--- src/books.js
+++ src/books.js
@@ -1,20 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
 export const books = [
   {
     author: 'Alan Alickovic',
     title: 'React Application Architecture for Production',
-    img: './images/book-1.jpg',
+    img: img1,
     id: 1,
   },
   {
     author: 'Carl Rippon',
     title: 'Learn React with TypeScript',
-    img: './images/book-2.jpg',
+    img: img2,
     id: 2,
   },
   {
     author: 'Theophilus Edet',
     title: 'React Web Development',
-    img: './images/book-3.jpg',
+    img: img3,
     id: 3,
   },
 ]
