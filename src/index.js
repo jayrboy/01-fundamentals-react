f407f5218ed1c65d40363f9ce3d34d9ca21c62b8
jayrboy <j.jayox_@hotmail.com>
2024-01-07 23:01:41 +0700

    Component Feature

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
-      <EventExample />
       {books.map((book) => {
         return <Book {...book} key={book.id} />
       })}
@@ -28,32 +27,17 @@ const BookList = () => {
   )
 }
 
-const EventExample = () => {
-  return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={(e) => console.log(e.target.value)}
-          style={{ margin: '1rem' }}
-        />
-        <button type="submit">submit form</button>
-      </form>
-      <button type="button" onClick={(e) => console.log('click me')}>
-        click me
-      </button>
-    </section>
-  )
-}
-
 const Book = (props) => {
   const { img, title, author } = props
+  // console.log(props)
+  const displayTitle = () => {
+    console.log(title)
+  }
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>display title</button>
       <h4>{author}</h4>
     </article>
   )
